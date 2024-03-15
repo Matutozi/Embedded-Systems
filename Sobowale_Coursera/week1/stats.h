4423d34ff7dfe3450ce1a36404877ff37fa3bace
Sobowale Emmanuel <sobowalegz2@gmail.com>
2024-03-15 16:21:30 +0100

    added the prototypes for al the functions implentation

--- Sobowale_Coursera/week1/stats.h
+++ Sobowale_Coursera/week1/stats.h
@@ -1,40 +1,29 @@
-/******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
- *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
- *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
- */
+ * File: stats.h
+ * author : Emmanuel 
+ * Date: 02/02/2024
+ * Description: Implementation of statistical functions for analyzing arrays.
+ **/
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
- */
+//FUNCTIONS PROTOTYPES
 
+void print_statistics (unsigned char minimum, unsigned char maximum, float mean, unsigned char median);
+void print_array (unsigned char *array, unsigned int length);
+unsigned char find_median(unsigned char *array, unsigned int length);
+
+int find_mean(unsigned char *array, unsigned int length); //  Given an array of data and a length, returns the mean
+
+unsigned char maximum(unsigned char *array, unsigned int length); //  Given an array of data and a length, returns the maximum
+
+unsigned char minimum(unsigned char *array, unsigned int length); //  Given an array of data and a length, returns the minumum
+
+
+void sort_array();
 
 #endif /* __STATS_H__ */
