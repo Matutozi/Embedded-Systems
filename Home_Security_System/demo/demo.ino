a4e3daca46685629c5a8224fbd60a8fc2e4f023e
Matutozi <sobowalegz2@gmail.com>
2024-04-28 14:03:12 +0100

    calculated the distance in inches

--- Home_Security_System/demo/demo.ino
+++ Home_Security_System/demo/demo.ino
@@ -2,7 +2,8 @@
 int trigPin = 12;
 int echoPin = 13;
 int pingTravelTime;
-
+float pingTravelDistance;
+float distance_to_target;
 void setup() {
   // put your setup code here, to run once:
   pinMode(trigPin, OUTPUT);
@@ -18,8 +19,15 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   pingTravelTime = pulseIn(echoPin, HIGH);
-  Serial.println(pingTravelTime);
   delay(30);
-
+  pingTravelDistance = (pingTravelTime * 765. * 5280. * 12) / (3600. * 1000000);
+  
+  distance_to_target = pingTravelDistance / 2;
+  Serial.print("Your distance to the target is: ");
+  
+  Serial.print(distance_to_target);
+  Serial.println(" inches.");
+  delay(500);
+ 
 
 }
