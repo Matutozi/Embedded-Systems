af54a04169871dd896623a2d9480dad98180c67c
Matutozi <sobowalegz2@gmail.com>
2024-04-30 01:04:33 +0100

    added some new prototypes for added functionality to my system

--- Home_Security_System/demo/main.h
+++ Home_Security_System/demo/main.h
@@ -3,6 +3,10 @@
 
 #include <LiquidCrystal.h>
 
-float measure_distance();
-void display_distance(float distance_to_target);
+float measure_distance(); //calculates the distance using the H2-Sr04
+void display_distance(float distance_to_target); //function that displays result on the LCD1602 display
+void setupRemote(); // function that control the remote control
+bool motion_sensing(); //pir sensor
+float temp_monitoring(); //monitors the temperature of the house
+bool smoke_sensor(); //returns a value based on some set parameters in the house
 #endif
